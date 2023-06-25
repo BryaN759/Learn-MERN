3e8f43b3fa4d2358bb98c9b479cb211111fee2d7
BryaN759 <khaledmushahed@gmail.com>
2023-06-25 15:11:06 +0600

    some mod

--- 12th-http-modules.js
+++ 12th-http-modules.js
@@ -8,9 +8,11 @@ const http = require('http')
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
         res.end('Welcome to our home page')
+        return
     }
     if (req.url === '/about'){
         res.end('Here is the about page')
+        return
     }
     res.end(`
     <h1>Oops!</h1>
@@ -19,4 +21,13 @@ const server = http.createServer((req,res)=>{
     `)
 })
 
-server.listen(5000)
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+  });
+  
+  
+  
+  
+  
+  
+  
